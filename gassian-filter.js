04c04e1a347a8fd40c0bb49d3d9519309b66b973
GodXero <godxero.dev@gmail.com>
2025-05-18 12:02:45 +0530

    Done

--- gassian-filter.js
+++ gassian-filter.js
@@ -1,3 +1,9 @@
+const gussianKernel = [
+	[1, 2, 1],
+	[2, 4, 2],
+	[1, 2, 1]
+];
+
 function getImageData (imageData) {
 	const width = imageData.width;
 	const height = imageData.height;
@@ -19,7 +25,7 @@ function getImageData (imageData) {
 	return pixels;
 }
 
-function getImageFromData (pixels) {
+function getImageFromPixelsData (pixels) {
 	const width = pixels[0].length;
 	const height = pixels.length;
 
@@ -53,7 +59,7 @@ function getImageFromData (pixels) {
 }
 
 function toBase64 (sourceImage) {
-	return new Promise((resolve, reject) => {
+	return new Promise((resolve, _) => {
 		const image = new Image();
 
 		image.crossOrigin = 'anonymous';
@@ -75,8 +81,64 @@ function toBase64 (sourceImage) {
 	});
 }
 
+function applyGassianForPixel (x, y, pixels, newPixels) {
+	let tr = 0;
+	let tg = 0;
+	let tb = 0;
+	let ta = 0;
+
+	for (let a = -1; a < 2; a++) {
+		for (let b = -1; b < 2; b++) {
+			let px = x + b;
+			let py = y + a;
+
+			if (px < 0) px = 0;
+			if (px > pixels[0].length - 1) px = pixels[0].length - 1;
+			if (py < 0) py = 0;
+			if (py > pixels.length - 1) py = pixels.length - 1;
+
+			const kernel = gussianKernel[a + 1][b + 1];
+			const pixel = pixels[py][px];
+
+			tr += pixel.r * kernel;
+			tg += pixel.g * kernel;
+			tb += pixel.b * kernel;
+			ta += pixel.a * kernel;
+		}
+	}
+
+	const newPixel = newPixels[y][x];
+	const kernelWeight = gussianKernel.flat().reduce((a, b) => a + b, 0);
+
+	newPixel.r = Math.floor(tr / kernelWeight);
+	newPixel.g = Math.floor(tg / kernelWeight);
+	newPixel.b = Math.floor(tb / kernelWeight);
+	newPixel.a = Math.floor(ta / kernelWeight);
+}
+
+function applyGassianForPixels (pixels) {
+	const width = pixels[0].length;
+	const height = pixels.length;
+	const newPixels = Array.from({ length: height }, (_, y) => Array.from({ length: width }, (_, x) => {
+		const pixel = pixels[y][x];
+
+		return {
+			r: pixel.r,
+			g: pixel.g,
+			b: pixel.b,
+			a: pixel.a
+		};
+	}));
+
+	for (let y = 0; y < height; y++)
+		for (let x = 0; x < width; x++)
+			applyGassianForPixel(x, y, pixels, newPixels);
+
+	return newPixels;
+}
+
 function gassianFilter (imageData) {
-	return getImageFromData(getImageData(imageData));
+	return getImageFromPixelsData(applyGassianForPixels(getImageData(imageData)));
 }
 
 export {
--- sketch.js
+++ sketch.js
@@ -1,25 +1,69 @@
 import { gassianFilter, toBase64 } from "./gassian-filter.js";
 
 const ctx = canvas.getContext('2d');
+const imgOriginal = new Image();
+
+imgOriginal.src = 'https://picsum.photos/500/300';
+
 const image = new Image();
+let filteredImage = null;
+
+let mouseX = 0;
+let mouseY = 0;
+
+function resize () {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+}
 
 function draw () {
-	ctx.drawImage(image, 0, 0, image.width, image.height);
+	requestAnimationFrame(draw);
 
-	const imageData = ctx.getImageData(0, 0, image.width, image.height);
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-	const filteredImage = gassianFilter(imageData);
+	if (!filteredImage) return;
 
-	ctx.drawImage(filteredImage, 0, image.height + 20, image.width, image.height);
+	ctx.drawImage(image, 0, 0, mouseX, image.height, 0, 0, mouseX, image.height);
+	ctx.drawImage(filteredImage, mouseX, 0, image.width - mouseX, image.height, mouseX, 0, image.width - mouseX, image.height);
+
+	ctx.strokeStyle = '#ffffff';
+	ctx.lineWidth = 2;
+
+	ctx.beginPath();
+	ctx.moveTo(mouseX, 0);
+	ctx.lineTo(mouseX, canvas.height);
+	ctx.stroke();
+}
+
+draw();
+resize();
+
+function getFilteredImage () {
+	const canvasTmp = document.createElement('canvas');
+	const ctxTmp = canvasTmp.getContext('2d');
+
+	canvasTmp.width = image.width;
+	canvasTmp.height = image.height;
+
+	ctxTmp.drawImage(image, 0, 0, image.width, image.height);
+
+	const imageData = ctxTmp.getImageData(0, 0, image.width, image.height);
+
+	return gassianFilter(imageData);
 }
 
 imgOriginal.addEventListener('load', async () => {
+	image.addEventListener('load', () => {
+		filteredImage = getFilteredImage();
+	});
+
 	image.src = await toBase64(imgOriginal);
 
-	image.addEventListener('load', () => {
-		canvas.width = image.width;
-		canvas.height = image.height * 2 + 20;
-
-		draw();
-	});
 });
+
+window.addEventListener('mousemove', event => {
+	mouseX = event.x;
+	mouseY = event.y;
+});
+
+window.addEventListener('resize', resize);
